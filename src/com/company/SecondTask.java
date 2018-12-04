fcd428e2dcc70c6a5733852c9ce215ecd1c2ca5c
Влад <Sparkjet385@gmail.com>
2018-12-04 21:04:43 +0200

    Initial commit

---
+++ src/com/company/SecondTask.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+
+public class SecondTask {
+
+    public static void main(String[] args) {
+
+        System.out.print("Введите строку -> ");
+        Scanner scan = new Scanner(System.in);
+        String str = new String(scan.nextLine());
+
+        Scanner firstCoordinate = new Scanner(System.in);
+        System.out.println("Введите координаты первой подстроки \n Номер первой буквы: ");
+        int x = firstCoordinate.nextInt() + 1;
+        System.out.println("Номер второй буквы: ");
+        int y = firstCoordinate.nextInt() + 1;
+        System.out.println("Введите координаты второй подстроки \n Номер первой буквы: ");
+        int b = firstCoordinate.nextInt() + 1;
+        System.out.println("Номер второй буквы: ");
+        int c = firstCoordinate.nextInt() + 1;
+
+        str = str.toLowerCase();
+
+        str = str.replace(str.substring(x,y), str.substring(b,c));
+
+        StringBuffer str_buffer = new StringBuffer(str.subSequence(0, str.length()));  //Замена String на StringBuffer
+
+        System.out.println("Строка с заменой подстрок -> " + str_buffer);
+    }
+
+}
